5c09a148d1b0fb009072e06d730318bca2f90dc7
Nipun Ravisara <ravisara.dev@gmail.com>
2024-04-20 08:17:56 +0530

    remove old nvim configs

---
+++ .config/nvim.old/lua/zenin/lazy.lua
@@ -0,0 +1,27 @@
+-- Install lazyvim plugin manager if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- setup plugins
+require("lazy").setup({ { import = "zenin.plugins" } }, {
+	install = {
+		colorscheme = { "catppuccin" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
---
+++ .config/nvim.old/lua/zenin/plugins/colorshceme.lua
@@ -0,0 +1,10 @@
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	name = "tokyonight",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd.colorscheme("tokyonight-night")
+-- 		vim.cmd.hi("Comment gui=none")
+-- 	end,
+-- }
