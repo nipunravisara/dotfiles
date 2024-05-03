2708801696937a16b9008559dadbc73a0cfb46b6
Nipun Ravisara <ravisara.dev@gmail.com>
2024-05-03 09:00:36 +0530

    new color scheme catppuccin

--- .config/nvim/lua/nipun/plugins/bufferline.lua
+++ .config/nvim/lua/nipun/plugins/bufferline.lua
@@ -8,4 +8,9 @@ return {
       separator_style = "slant",
     },
   },
+  config = function()
+    require("bufferline").setup({
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+    })
+  end,
 }
