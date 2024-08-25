3c88f09be61831510a124fc849462a530952e4ae
Nipun Ravisara <ravisara.dev@gmail.com>
2024-08-25 21:48:31 +0530

    update true colors settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,3 @@
 require("zenin.core")
 require("zenin.lazy")
+vim.opt.termguicolors = true
--- .config/nvim/lua/zenin/plugins/colorscheme.lua
+++ .config/nvim/lua/zenin/plugins/colorscheme.lua
@@ -1,28 +1,29 @@
 return {
-  "catppuccin/nvim",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "macchiato",
-      background = {
-        dark = "mocha",
-      },
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-      },
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        mason = true,
-        telescope = {
-          enabled = true,
-        },
-      },
-      transparent_background = false,
-    })
+	"catppuccin/nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato",
+			background = {
+				dark = "mocha",
+			},
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+			},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				mason = true,
+				telescope = {
+					enabled = true,
+				},
+			},
+			transparent_background = true,
+		})
 
-    vim.cmd("colorscheme catppuccin")
-  end,
+		vim.cmd("colorscheme catppuccin")
+	end,
 }
