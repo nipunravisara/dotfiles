219c81d3059073f048c6be6acf09ed75cdefa3ef
nipunravisara <ravisara.dev@gmail.com>
2025-05-07 23:07:14 +0530

    install spaceship

--- .config/nvim/lua/zenin/plugins/colorscheme.lua
+++ .config/nvim/lua/zenin/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"ayu-theme/ayu-vim",
-	priority = 1000,
-	config = function()
-		vim.g.ayucolor = "mirage"
-		vim.cmd([[colorscheme ayu]])
-	end,
+    "ayu-theme/ayu-vim",
+    priority = 1000,
+    config = function()
+        vim.g.ayucolor = "mirage"
+        vim.cmd([[colorscheme ayu]])
+    end,
 }
