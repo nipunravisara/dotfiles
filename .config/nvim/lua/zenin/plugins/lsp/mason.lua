3088c3436b45356f98c316b34fe22bc94e8ee02a
nipunravisara <ravisara.dev@gmail.com>
2026-03-19 22:29:42 +0530

    remove old wallpapers

--- .config/nvim/lua/zenin/plugins/lsp/mason.lua
+++ .config/nvim/lua/zenin/plugins/lsp/mason.lua
@@ -14,8 +14,6 @@ return {
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local capabilities = cmp_nvim_lsp.default_capabilities()
         local default_lsp_servers = {
-            "ts_ls",
-            "html",
             "cssls",
             "tailwindcss",
             "lua_ls",
